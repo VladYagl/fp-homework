a13498674c11055a6e8b5cbea025ed56aa2508cf
Vlad <VladYaglamunov@gmail.com>
2018-05-11 03:14:02 +0300

    Where is my mind

--- hw3/src/Operators.hs
+++ hw3/src/Operators.hs
@@ -28,35 +28,37 @@ data Oper = Init VarName Expr
 interpret :: (MonadError LangError m, MonadState VarEnv m, MonadIO m, MonadCont m) => [Oper] -> m ()
 interpret list = callCC $ \stop -> doSteps 1 list stop
   where
-    doSteps pos (x:xs) stop = do
-        step x stop `catchError` (\e -> throwError $ InterpretError e pos)
-        doSteps (pos + 1) xs stop
     doSteps _ [] _ = return ()
+    doSteps pos (x:xs) stop = do
+        step x `catchError` (\e -> throwError $ InterpretError e pos)
+        doSteps (pos + 1) xs stop
 
-    step (Init name expr) _ = do
-        vars <- get
-        value <- runReaderT (eval expr) vars
-        initialize name value
-    step (Set name expr) _ = do
-        vars <- get
-        value <- runReaderT (eval expr) vars
-        set name value
-    step (Read name) _ = do
-        value <- liftIO readLn
-        initialize name value
-    step (Print expr) _ = do
-        vars <- get
-        value <- runReaderT (eval expr) vars
-        liftIO $ print value
-    step Break stop = stop ()
-    step (For var fromExpr toExpr opers) _ = do
-        vars <- get
-        from <- runReaderT (eval fromExpr) vars
-        to <- runReaderT (eval toExpr) vars
-        for from to vars
       where
-        for cur to vars
-          | cur <= to = do
-              result <- runStateT (interpret opers) (Map.insert var cur vars)
-              for (cur + 1) to (snd result)
-          | otherwise = put vars
+        step (Init name expr) = do
+            vars <- get
+            value <- runReaderT (eval expr) vars
+            initialize name value
+        step (Set name expr) = do
+            vars <- get
+            value <- runReaderT (eval expr) vars
+            set name value
+        step (Read name) = do
+            value <- liftIO readLn
+            initialize name value
+        step (Print expr) = do
+            vars <- get
+            value <- runReaderT (eval expr) vars
+            liftIO $ print value
+        step Break =
+            stop ()
+        step (For var fromExpr toExpr opers) = do
+            vars <- get
+            from <- runReaderT (eval fromExpr) vars
+            to <- runReaderT (eval toExpr) vars
+            for from to vars
+          where
+            for cur to vars
+              | cur <= to = do
+                  result <- runStateT (interpret opers) (Map.insert var cur vars)
+                  for (cur + 1) to (snd result)
+              | otherwise = put vars
